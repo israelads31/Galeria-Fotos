f0581a758a790ef2fd6d87aa2e54227286da3b88
Israel Aranha <israelaranhads31@gmail.com>
2024-04-09 14:58:55 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $('header button').click(function () {
+    $('form').slideDown();
+  })
+
+  $('#botao-cancelar').click(function () {
+    $('form').slideUp();
+  })
+
+  $('form').on('submit', function (e) {
+    e.preventDefault();
+    const enderecoImagem = $('#endereco-imagem').val();
+    const novoItem = $('<li style="display: none;"></li>');
+    $(`<img src="${enderecoImagem}" />`).appendTo(novoItem);
+    $(`<div class="overlay-imagem-link">
+        <a href="${enderecoImagem}" target="_blank" title="Ver imagem em tamanho real">Ver imagem em tamanho real</a>
+      </div>
+    `).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
+    $(novoItem).fadeIn(1000);
+    $('#endereco-imagem').val('');
+  })
+})
